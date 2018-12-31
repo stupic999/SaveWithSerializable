3e8d2baa0a2ae306dd2931d56272bba1535f756d
stupid999 <aierzhiguang2@gmail.com>
2018-12-31 20:50:02 +0800

    学习记录： 能用Serializable储存游戏资料

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour {
+
+    public int num = 0;
+
+    [SerializeField]
+    private Text text;
+
+    public void SaveGame()
+    {
+        Save save = CreateSaveGameObject();
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+        num = 0;
+        text.text = num.ToString();
+
+        Debug.Log("Game Saved");
+    }
+
+    private Save CreateSaveGameObject()
+    {
+        Save save = new Save();
+
+        save.num = num;
+
+        return save;
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            Save save = (Save)bf.Deserialize(file);
+            file.Close();
+
+            num = save.num;
+            text.text =save.num.ToString();
+
+            Debug.Log("Game Loaded");
+        }
+        else
+        {
+            Debug.Log("No game saved!");
+        }
+    }
+
+    public void Click()
+    {
+        num++;
+        text.text = num.ToString();
+    }
+}
